ec73cd5f1f44393a739c925af4a5641f51a60d94
raajnasir <70377100+raajnasir@users.noreply.github.com>
2022-12-02 19:55:35 +0530

    Add files via upload

---
+++ content_filtering.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwis import cosine_similarity
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('final_csv')
+df = df[df['soup'].notna()]
+
+count = CountVectorizer(stop_words = 'english')
+count_matrix = count.fit_transform(df['soup'])
+
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+df = df.reset_index()
+indices = pd.Series(df.index, index = df['title'])
+
+def get_recommendations(title): 
+    idx = indices[title]
+    sim_scores = sim_scores[1 : 11]
+    movie_indices = [i[0] for i in sim_scores]
+    return df[['title', 'poster_link', 'release_date', 'runtime', 'vote_average', 'overview']].iloc[movie_indices].values.tolist()
+
+    
+
